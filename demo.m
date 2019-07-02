56b3fc89a921dd7736ebe24bc25a84c38d50aaef
Mahmoud Afifi <37669469+mahmoudnafifi@users.noreply.github.com>
2019-07-01 23:27:26 -0400

    Add files via upload

--- demo.m
+++ demo.m
@@ -1,18 +1,25 @@
 %demo
-image_name = 'Canon1DsMkIII_0095.png'; %image name 
-%Note: be sure that black level subtraction and saturation level-based normalization are applied to the image before using it
+image_name = fullfile('imgs','GehlerShi_Canon5d_IMG_0883.png'); %image name 
+%Note: be sure that the image is in the sraw-RGB linear space and the 
+%black/saturation normalization are correctly applied to the image before 
+%using it.
 
 model_name = 'trained_model_wo_NUS_Canon1DsMkIII'; %trained model name
+%model_name = 'trained_model_wo_NUS_Canon1DsMkIII'; %trained model name
 in_img_sz = 150; %our network accepts 150x150 raw-RGB image
 load(fullfile('models',model_name)); %load the trained model
-I = imresize(imread(image_name),[in_img_sz,in_img_sz]); %resize the image
+I = imread(image_name); %resize the image
+sz =size(I);
+if sz(1)~=in_img_sz || sz(2)~=in_img_sz
+    I = imresize(I,[in_img_sz,in_img_sz]); %resize the image
+end
 est_ill = predict(trained_model,I); %estimate the scene illuminant
 est_ill =  est_ill./norm(est_ill); %make it a unit vector
 fprintf('Estimated scene illuminant =  %f, %f, %f\n',...
     est_ill(1),est_ill(2),est_ill(3)); %display the result
 
-subplot(1,2,1); imshow(I*4); title('input raw-RGB img'); %show input raw-RGB image (here, we scale it by 4 to aid visualization)
+subplot(1,2,1); imshow(I*4); title('Input raw-RGB image'); %show input raw-RGB image (here, we scale it by 4 to aid visualization)
 subplot(1,2,2); imshow(reshape(...
     reshape(im2double(I),[],3)*diag(est_ill(2)./est_ill),size(I))*4);  %apply white balance correction then show the result (scaled to aid visualization)
-title('White-balanced raw-RGB img');
+title('White-balanced raw-RGB image');
 
